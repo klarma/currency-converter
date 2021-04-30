30c4908a9663172a2429168c7c8663e381d41071
Katarzyna Napierala <k_napierala@op.pl>
2021-04-30 12:29:19 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-form__inputPLN");
-let strongEURElement = document.querySelector(".js-section__EUR");
-let strongUSDElement = document.querySelector(".js-section__USD");
-let strongGBPElement = document.querySelector(".js-section__GBP");
-let buttonReset = document.querySelector(".js-form__buttonReset");
+{
+    const strongEURElement = document.querySelector(".js-section__EUR");
+    const strongUSDElement = document.querySelector(".js-section__USD");
+    const strongGBPElement = document.querySelector(".js-section__GBP");
 
-formElement.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let pln = +amountElement.value;
-    let eurRate = pln / 4.57;
-    let usdRate = pln / 3.82;
-    let gbpRate = pln / 5.26;
-    strongEURElement.innerText = `${eurRate.toFixed(2)} €`;
-    strongUSDElement.innerText = `${usdRate.toFixed(2)} $`;
-    strongGBPElement.innerText = `${gbpRate.toFixed(2)} £`;
-})
+    const welcome = () => {
+        console.log("Hello world created by Krzysiu. Nice to be here");
+    }
 
-buttonReset.addEventListener("click", ()=>{
-    strongEURElement.innerText="";
-    strongUSDElement.innerText="";
-    strongGBPElement.innerText="";
-})
+    const showResults = (eurRate, usdRate, gbpRate) => {
+        strongEURElement.innerText = `${eurRate.toFixed(2)} €`;
+        strongUSDElement.innerText = `${usdRate.toFixed(2)} $`;
+        strongGBPElement.innerText = `${gbpRate.toFixed(2)} £`;
+    }
 
+    const onSubmitCalculation = e => {
+        const amountElement = document.querySelector(".js-form__inputPLN");
+        e.preventDefault();
+        const pln = +amountElement.value;
+        const eurRate = pln / 4.57;
+        const usdRate = pln / 3.82;
+        const gbpRate = pln / 5.26;
+        showResults(eurRate, usdRate, gbpRate);
+    }
 
+    const resetForm = () => {
+        strongEURElement.innerText = "";
+        strongUSDElement.innerText = "";
+        strongGBPElement.innerText = "";
+    }
 
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const buttonReset = document.querySelector(".js-form__buttonReset");
+        formElement.addEventListener("submit", onSubmitCalculation);
+        buttonReset.addEventListener("click", resetForm);
+        welcome();
+    }
+
+    init()
+
+}
