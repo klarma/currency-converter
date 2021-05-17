a8ab7ac8a3b9be7e4c6211877deda833782e1b1a
Katarzyna Napierala <k_napierala@op.pl>
2021-05-17 20:07:05 +0200

    currency selection added and styles adapted

--- js/script.js
+++ js/script.js
@@ -1,43 +1,50 @@
 {
-    const strongEURElement = document.querySelector(".js-section__EUR");
-    const strongUSDElement = document.querySelector(".js-section__USD");
-    const strongGBPElement = document.querySelector(".js-section__GBP");
     const amountElement = document.querySelector(".js-form__inputPLN");
+    const resultElement = document.querySelector(".js-section__paragraph");
 
     const welcome = () => {
         console.log("Hello world created by Krzysiu. Nice to be here");
-    }
+    };
 
-    const showResults = (eurRate, usdRate, gbpRate) => {
-        strongEURElement.innerText = `${eurRate.toFixed(2)} €`;
-        strongUSDElement.innerText = `${usdRate.toFixed(2)} $`;
-        strongGBPElement.innerText = `${gbpRate.toFixed(2)} £`;
-    }
+    const showResults = (pln, result, currency) => {
+        resultElement.innerHTML = `${pln} zł to <strong>${result.toFixed(2)} ${currency}</strong>`;
+    };
 
-    const onSubmitCalculation = e => {   
+    const onSubmitCalculation = e => {
         e.preventDefault();
+
         const pln = +amountElement.value;
-        const eurRate = pln / 4.57;
-        const usdRate = pln / 3.82;
-        const gbpRate = pln / 5.26;
-        showResults(eurRate, usdRate, gbpRate);
-    }
+        const currency = document.querySelector(".js-form__select").value;
+
+        let result;
+
+        switch (currency) {
+            case "EUR":
+                result = pln / 4.57;
+            case "USD":
+                result = pln / 3.82;
+            case "GBP":
+                result = pln / 5.26;
+        };
+
+        showResults(pln, result, currency);
+    };
 
     const resetForm = () => {
-        strongEURElement.innerText = "";
-        strongUSDElement.innerText = "";
-        strongGBPElement.innerText = "";
+        resultElement.innerText = "";
         amountElement.focus();
     }
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
         const buttonReset = document.querySelector(".js-form__buttonReset");
+
         formElement.addEventListener("submit", onSubmitCalculation);
         buttonReset.addEventListener("click", resetForm);
+
         welcome();
     }
 
-    init()
+    init();
 
 }
